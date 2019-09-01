8bf54b112ec0fbfb7be203a91cf75d5f5de82715
Ayk Borstelmann <borstelmann.ayk@gmail.com>
2019-09-01 14:27:08 +0200

    Init Spring with Spring Boot

---
+++ src/main/java/com/bjs/bjsapi/BjsApiApplication.java
@@ -0,0 +1,13 @@
+package com.bjs.bjsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BjsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BjsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bjs/bjsapi/BjsApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bjs.bjsapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BjsApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
