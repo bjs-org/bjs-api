99d55779665d0f29c47e2beb8c83455761ea88c0
skyliam <liamskydamien@gmail.com>
2019-09-28 16:38:54 +0200

    Implement basic calculation

--- api/src/main/java/com/bjs/bjsapi/database/model/enums/DisciplineType.java
+++ api/src/main/java/com/bjs/bjsapi/database/model/enums/DisciplineType.java
@@ -5,18 +5,38 @@ package com.bjs.bjsapi.database.model.enums;
  */
 public enum DisciplineType {
 
-	RUN_50,
-	RUN_75,
-	RUN_100,
-	RUN_800,
-	RUN_2000,
-	RUN_3000,
+	RUN_50(true,50),
+	RUN_75(true,75),
+	RUN_100(true,100),
+	RUN_800(true,800),
+	RUN_2000(true,2000),
+	RUN_3000(true,3000),
 
-	HIGH_JUMP,
-	LONG_JUMP,
-	SHOT_PUT,
-	SLING_BALL,
-	BALL_THROWING_80,
-	BALL_THROWING_200;
+	HIGH_JUMP(false),
+	LONG_JUMP(false),
+	SHOT_PUT(false),
+	SLING_BALL(false),
+	BALL_THROWING_80(false),
+	BALL_THROWING_200(false);
+
+	private final boolean isRun;
+	private int distance;
+
+	public int getDistance() {
+		return distance;
+	}
+
+	DisciplineType(boolean isRun, int distance) {
+		this.isRun = isRun;
+		this.distance = distance;
+	}
+
+	DisciplineType(boolean isRun) {
+		this.isRun = isRun;
+	}
+
+	public boolean isRUN(){
+		return isRun;
+	}
 
 }
