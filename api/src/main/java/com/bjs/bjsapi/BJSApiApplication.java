07e388002669c213a6964d8fc8e2ea6d235b4e72
Ayk Borstelmann <borstelmann.ayk@gmail.com>
2019-11-24 14:53:57 +0100

    Fix config bug

--- api/src/main/java/com/bjs/bjsapi/BJSApiApplication.java
+++ api/src/main/java/com/bjs/bjsapi/BJSApiApplication.java
@@ -2,8 +2,12 @@ package com.bjs.bjsapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.bjs.bjsapi.config.CalculationInformationConfig;
 
 @SpringBootApplication
+@EnableConfigurationProperties(CalculationInformationConfig.class)
 public class BJSApiApplication {
 
 	public static void main(String[] args) {
