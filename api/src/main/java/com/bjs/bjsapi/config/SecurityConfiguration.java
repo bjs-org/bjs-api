7d3e0bfd323c3b2b8855cc5ab35aeb4e33650a9a
Ayk Borstelmann <borstelmann.ayk@gmail.com>
2020-01-28 18:22:13 +0100

    Edit cors configuration

--- api/src/main/java/com/bjs/bjsapi/config/SecurityConfiguration.java
+++ api/src/main/java/com/bjs/bjsapi/config/SecurityConfiguration.java
@@ -1,5 +1,7 @@
 package com.bjs.bjsapi.config;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -11,6 +13,9 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.bjs.bjsapi.security.BJSUserDetailsService;
 
@@ -45,7 +50,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.httpBasic()
+		http.cors()
+			.and()
+			.httpBasic()
 			.and()
 			.authorizeRequests()
 			.mvcMatchers("/api/v1/**").authenticated()
@@ -54,6 +61,18 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 			.sessionManagement().disable()
 			.rememberMe().disable()
 			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}
+
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList("*"));
+		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+		configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type"));
+		configuration.setAllowCredentials(true);
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
 	}
 
 }
